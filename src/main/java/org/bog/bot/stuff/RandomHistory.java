79a35a2a1612b34628c663724f0f6adcc26bda37
Luke <luke.tallon@gmail.com>
2023-09-21 01:11:33 +0100

    0.1.0-SNAPSHOT refactored code. Improved code to non-blocking async retrieval of messages.

--- src/main/java/org/bog/bot/stuff/RandomHistory.java
+++ src/main/java/org/bog/bot/stuff/RandomHistory.java
@@ -11,64 +11,73 @@ import java.io.PrintWriter;
 import java.time.OffsetDateTime;
 
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 public class RandomHistory {
 
     private static final Logger logger = LoggerFactory.getLogger(RandomHistory.class);
-    private final Map<Long, List<Message>> channelMessageHistories = new HashMap<>();
+    private final CharSequence LOADING_MESSAGE = "Loading messages, please wait...";
+    private final String MessageFile = "AllMessageIds.txt";
+    private final Map<Long, List<Message>> channelMessageHistories = new ConcurrentHashMap<>();
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm - dd/MM/yyyy");
-    private final int MAX_MESSAGES = 110000;        //current likely max of our group chat in the near future.
-    private int totalMessages;
-    private List<Message> messageList;
 
     public String getRandomQuote(TextChannel channel) {
 
         if (channelMessageHistories.get(channel.getIdLong()) != null) {
             return quoteRandomMessage(channelMessageHistories.get(channel.getIdLong()));
-        }else {
+        } else {
             return "No messages in memory, please use '!loadMessages' to populate BogBot's sack";
         }
     }
-    public void populateMessages(TextChannel channel){
 
-        if(!channelMessageHistories.containsKey(channel.getIdLong())) {
+    public void populateMessages(TextChannel channel) {
+        if (!channelMessageHistories.containsKey(channel.getIdLong())) {
             CompletableFuture<List<Message>> allMessages = retrieveAllMessages(channel);
-            logger.info("interesting: {}",allMessages);
-            allMessages.thenApply(list -> list.stream().collect(Collectors.toList()))
-                    .thenAccept(messages -> channelMessageHistories.put(channel.getIdLong(), messages));
-            setTotalMessages(channelMessageHistories.get(channel.getIdLong()).size());
-            logger.info("finale final size: {}", totalMessages);
-            writeHistListToFile(channelMessageHistories.get(channel.getIdLong()));
+
+            channel.sendMessage(LOADING_MESSAGE).queue(loadingMessage -> allMessages.thenAccept(messages -> {
+                channelMessageHistories.put(channel.getIdLong(), messages);
+                logger.info("finale final size: {}", totalMessageCount(channel));
+                writeAllMessageIdsToFile(messages);
+                loadingMessage.editMessage("Loading finished... " + totalMessageCount(channel) + " messages retrieved.").queue();
+            }));
         }
     }
 
     private CompletableFuture<List<Message>> retrieveAllMessages(TextChannel channel) {
-        try {
-            logger.info("Beginning message retrieval in channel: {}", channel.getIdLong());
-            logger.info("This may take some time if the channel has a large number of messages.");
+        logger.info("Beginning message retrieval in channel: {}", channel.getIdLong());
+        logger.info("This may take some time if the channel has a large number of messages.");
+        AtomicInteger count = new AtomicInteger();
+        List<Message> messages = new ArrayList<>();
 
-            return channel.getIterableHistory()
-                    .takeAsync(MAX_MESSAGES);
-        } catch (Exception e) {
-            logger.error("Error retrieving messages:", e);
-            return CompletableFuture.completedFuture(Collections.emptyList()); // Return an empty list in case of error
-        }
+        return channel.getIterableHistory()
+                .forEachAsync(message -> {
+                    aSyncMessagePopulate(message, messages, count);
+                    return true; // Continue iteration for all messages
+                })
+                .thenApply(result -> messages)
+                .exceptionally(e -> {
+                    logger.error("Error retrieving messages:", e);
+                    return Collections.emptyList();
+                });
     }
 
+    private void writeAllMessageIdsToFile(List<Message> AllMessageIds) {
 
-
-    private void writeHistListToFile(List<Message> histlist) {
-        try (PrintWriter writer = new PrintWriter("histlist.txt")) {
-            for (Message message : histlist) {
+        try (PrintWriter writer = new PrintWriter(MessageFile)) {
+            for (Message message : AllMessageIds) {
                 writer.println(message.getIdLong());
             }
-            logger.info("histlist contents written to histlist.txt");
+            logger.info("All Message Ids written to" + MessageFile);
         } catch (FileNotFoundException e) {
-            logger.error("Error writing histlist contents to file", e);
+            logger.error("Error writing All Message Ids contents to file", e);
             throw new RuntimeException(e);
         }
     }
@@ -81,14 +90,8 @@ public class RandomHistory {
 
         StringBuilder quotedMessage = new StringBuilder();
 
-        // Quote message content
-        quotedMessage.append("> ").append(boldenText(randomMessage.getContentRaw())).append("\n");
-
-        // Quote author
-        quotedMessage.append("- Posted by: ").append(randomMessage.getAuthor().getEffectiveName()).append("\n");
-
-        // Quote date
-        quotedMessage.append("- Date: ").append(nicelyFormattedDateTime(randomMessage.getTimeCreated())).append("\n");
+        // Format message content, author, and date
+        formatMessageDetails(quotedMessage, randomMessage);
 
         // Check and quote attachments
         for (Message.Attachment attachment : randomMessage.getAttachments()) {
@@ -101,6 +104,24 @@ public class RandomHistory {
         quotedMessage.append("Link: ").append(randomMessage.getJumpUrl()).append("\n");
 
         return quotedMessage.toString();
+    }
+
+    private void aSyncMessagePopulate(Message message, List<Message> messages, AtomicInteger count) {
+        messages.add(message);
+        count.getAndIncrement();
+        if (count.get() % 100 == 0) {
+            logger.info("Messages size: {}", messages.size());
+        }
+    }
+
+    private int totalMessageCount(TextChannel channel) {
+        return channelMessageHistories.get(channel.getIdLong()).size();
+    }
+
+    private void formatMessageDetails(StringBuilder builder, Message message) {
+        builder.append("> ").append(boldenText(message.getContentRaw())).append("\n")
+                .append("- Posted by: ").append(message.getAuthor().getEffectiveName()).append("\n")
+                .append("- Date: ").append(nicelyFormattedDateTime(message.getTimeCreated())).append("\n");
     }
 
     private String nicelyFormattedDateTime(OffsetDateTime originalDateTime) {
